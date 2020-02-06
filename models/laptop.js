9120102bc3a15ff5b3b6176e2cf3182b4caf5c74
yash45 <lebronash@gmail.com>
2020-02-06 08:40:30 +0300

    created a class laptop with laptopAge and adjPrice methods

--- models/laptop.js
+++ models/laptop.js
@@ -1,15 +1,41 @@
 class Laptop { 
-    constructor (brand, HDD, RAM, price){ 
-        this.name = brand;    
+    constructor (brand, HDD, RAM, price, manufactureYear){ 
+        this.brand = brand
         this.HDD = HDD;       
         this.RAM = RAM;
-        this.price = price
+        this.price = price;
+        this.manufactureYear = manufactureYear;
+        this.currentYear = new Date().getFullYear();
     }
-    toString(){
-        return 
+    checkBrand(){
+        if(typeof this.brand == "string"){
+            return true;   
+        }
+        else {
+            return "wrong input"
+        }
+    }
+
+    laptopAge(){
+        return (this.currentYear - this.manufactureYear)
+    }
+
+    adjPrice(){
+        const discount = this.price * 0.3
+            if(this.laptopAge() > 3){
+                return discount
+            }
+            else{
+                return this.price
+            }
+        }
+
+    printableString(){
+        return `Laptop Specification: \n {Name:${this.brand}\n RAM: ${this.RAM} \n HDD: ${this.HDD}\n Price: ${this.price}\n Date of Manufacture: ${this.DoM}}`
     }
 }
 
+module.exports = Laptop
 
 //home work: 
 // in the index.js----create an instance of the laptop class , a call the string method of that laptop, 
